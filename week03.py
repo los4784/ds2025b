2e8ee60f2c984138fc00003557114d31e194764e
los4784 <los4784@naver.com>
2025-03-24 11:31:25 +0900

    v0.6 array

--- week03.py
+++ week03.py
@@ -1,7 +1,27 @@
-l = [11, 9, -77, 8]
-for i, v in enumerate(l):
-    print(i, v)
+import  array
+def move_zeros(l):
+    zero_idx = 0
+    for i in range(len(l)):
+        n = l[i]
+        if n != 0:
+            l[zero_idx] = n
+            if zero_idx != i:
+                l[i] = 0
+            zero_idx = zero_idx + 1
+    return l
+
+
+arr = array.array('f', [11, 9, -77, 8])
+for i in range(len(arr)):
+    print(f"{arr[i]:3} {id(arr[i])}")
+l = [11, 0, 9, 0, -77]
+move_zeros(l)
+print(l)
+
+# l = [11, 9, -77, 8]
+# for i, v in enumerate(l):
+#     print(i, v)
 
 # l = [11, 9, -77, 8]
 # for i in range(len(l)):
-#print(i, l[i])
+#     print(i, l[i])
