3414e8a2c0f511868389875dc19d69b7a14087ea
los4784 <los4784@naver.com>
2025-03-24 11:38:54 +0900

    v0.7 array

--- week03.py
+++ week03.py
@@ -1,32 +1,6 @@
-def move_zeros(a_list):
-    zero_index = 0
-    for index, n in enumerate(a_list):
-        if n != 0:
-            a_list[zero_index] = n
-            if zero_index != index:
-                a_list[index] = 0
-            zero_index += 1
-    return(a_list)
+groups = ['HOT', 'Seventeen', 'Black Pink', 'NJZ']
+#ratings = [1,2,4,3, 100]
+ratings = [1,2,4,3]
 
-# def move_zeros(l):
-#     zero_idx = 0
-#     for i in range(len(l)):
-#         n = l[i]
-#         if n != 0:
-#             l[zero_idx] = n
-#             if zero_idx != i:
-#                 l[i] = 0
-#             zero_idx = zero_idx + 1
-#     return l
-
-a_list = [8, 0, 3, 0, 12]
-move_zeros(a_list)
-print(a_list)
-
-# l = [11, 9, -77, 8]
-#  for i, v in enumerate(l):
-#      print(i, v)
-#
-# # l = [11, 9, -77, 8]
-#  # for i in range(len(l)):
-#  #     print(i, l[i])
+groups_rating = list(zip(groups, ratings))
+print(groups_rating)
