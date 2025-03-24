5d4e7855bf1cb6deb6ee62a0510ad3f97bdb2e40
los4784 <los4784@naver.com>
2025-03-24 11:44:32 +0900

    v0.8 array

--- week03.py
+++ week03.py
@@ -1,6 +1,4 @@
-groups = ['HOT', 'Seventeen', 'Black Pink', 'NJZ']
-#ratings = [1,2,4,3, 100]
-ratings = [1,2,4,3]
-
-groups_rating = list(zip(groups, ratings))
-print(groups_rating)
+# city = ['Incheon', 'Incheon', 'Incheon', 'Gimpo', 'Seoul', 'Seoul']
+city = {'Incheon', 'Incheon', 'Incheon', 'Gimpo', 'Seoul', 'Seoul'}
+city = set(city)
+print(city)
