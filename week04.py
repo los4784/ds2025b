e256ba22c75aa9bdf77c83c4c8881ed583430f13
los4784 <los4784@naver.com>
2025-03-31 11:56:37 +0900

    v1.5 linked list

--- week04.py
+++ week04.py
@@ -1,3 +1,5 @@
+import random
+
 class Node:
     def __init__ (self, data, link=None):
         self.data = data
@@ -8,7 +10,7 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
-    def append(self,data):
+    def append(self, data):
         if not self.head:   # 링크리스트가 노드가 하나도 없는 상태면
             self.head = Node(data)  #  새 노드를 head 연결
             return
@@ -17,15 +19,34 @@ class LinkedList:
             current = current.link  # 다음노드를 이용
         current.link = Node(data)
 
-    def __str__(self):  #
+    def search(self, target):
+        current = self.head
+        while current.link:
+            if current.data == target:
+                return f"{target}을(를) 찾았습니다"
+            else:
+                current = current.link
+        return f"{target}은(는) 링크드리스트 안에 존재하지 않습니다"
+
+    def __str__(self):
         node = self.head
+        out_texts = ""
         while node is not None:
-            out_texts = out_texts + f"{node.data} +  -> "
+            out_texts = out_texts + f"{node.data} -> "
             node = node.link
-        return out_texts  + "end"
+        return out_texts + "end"
+
+# ll = LinkedList()
+# ll.append(8)
+# ll.append(10)
+# ll.append(-9)
+# print(ll)
+# print(ll.search(100))
+# print(ll.search(10))
 
 ll = LinkedList()
-ll.append(8)
-ll.append(10)
-ll.append(-9)
+for _ in range(10):
+    ll.append(random.randint(1, 30))
+
 print(ll)
+print(ll.search(10))
