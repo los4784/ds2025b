e874c7d227f4b7475b52ed9fcfb4e0eeae7c96a6
los4784 <los4784@naver.com>
2025-04-07 11:39:49 +0900

    v2.2 stack

--- week05.py
+++ week05.py
@@ -25,8 +25,10 @@ class Stack:
 
 
 s1 = Stack()
-print(s1.pop())
+#print(s1.pop())
 s1.push("Data structure")
 s1.push("Database")
-print(s1.pop())
-print(s1.pop())
+#print(s1.pop())
+#print(s1.pop())
+for i in range(3):
+    print(s1.pop())
