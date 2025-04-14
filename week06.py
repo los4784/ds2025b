d04513709c9b1945c126fee14d9c34357278bc14
los4784 <los4784@naver.com>
2025-04-14 12:09:47 +0900

    v2.9 queue

--- week06.py
+++ week06.py
@@ -1,42 +1,7 @@
-class Node:
-    def __init__(self, data, link=None):
-        self.data = data
-        self.link = link
-
-
-class Queue:
-    def __init__(self):
-        self.front = None
-        self.rear = None
-        self.size = 0
-
-    def enqueue(self, data):
-        self.size = self.size + 1
-        node = Node(data)
-        if self.rear is None:
-            self.front = node
-            self.rear = node
-        else:
-            self.rear.link = node
-            self.rear = node
-
-    def dequeue(self):
-        if self.front is None:
-            raise IndexError("Queue is empty")
-        self.size = self.size -1
-        temp = self.front
-        # temp.link = None 를 쓰면 밑에 있는 font는 큐의 연결이 끊어져서 문제가 발생함
-        self.front = self.front.link
-        if self.front is None:
-            self.rear = None
-        temp.link = None
-        return  temp.data
-
+from queue import  Queue
 
 q = Queue()
-q.enqueue("Database")
-q.enqueue("Data structure")
-print(q.size, q.front.data, q.rear.data)
-q.dequeue()
-q.dequeue()
-print(q.size, q.front, q.rear) # None이면 큐가 비었거나, 해당 위치에 노드가 없어서 .link로 연결된 다음 노드가 없다
+q.put("Database")
+q.put("Data structure")
+print(q.get())
+print(q.get())
