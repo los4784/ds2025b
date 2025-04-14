56ce64c80391e5dc3462157075072e5b77e025db
los4784 <los4784@naver.com>
2025-04-14 11:45:25 +0900

    v2.7 queue

--- week06.py
+++ week06.py
@@ -20,8 +20,21 @@ class Queue:
             self.rear.link = node
             self.rear = node
 
+    def dequeue(self):
+        if self.front is None:
+            raise IndexError("Queue is empty")
+        self.size = self.size -1
+        temp = self.front
+        self.front = self.front.link
+        if self.front is None:
+            self.rear = None
+        return  temp.data
+
 
 q = Queue()
 q.enqueue("Database")
 q.enqueue("Data structure")
 print(q.size, q.front.data, q.rear.data)
+q.dequeue()
+q.dequeue()
+print(q.size, q.front, q.rear) #None이면 큐가 비었거나, 해당 위치에 노드가 없어서 .link로 연결된 다음 노드가 없다
