568c2399250ed30a5e82a93e1f3f9149857ce6c4
los4784 <los4784@naver.com>
2025-05-12 11:37:25 +0900

    v3.8 tree

--- week10.py
+++ week10.py
@@ -64,19 +64,38 @@ def search(find_number):
             current = current.right
 
 
+def delete(node, value):
+    if node is None:
+        return None
+
+    if value < node.data:
+        node.left = delete(node.left, value)
+    elif value > node.data:
+        node.right = delete(node.right, value)
+    else:
+        # 삭제할 노드 발견
+        # 자식이 없는 leaf 노드거나 자식이 하나만 있는 경우
+        if node.left is None:
+            return node.right
+        elif node.right is None:
+            return node.left
+        # 자식이 2개인 경우
+    return node
+
+
 if __name__ == "__main__":
-    numbers = [10, 15, 8, 3, 9]
+    numbers = [10, 15, 8, 3, 9, 14]
     root = None
 
     for number in numbers:
         root = insert(root, number)
 
     print("BST 구성 완료")
-    post_order(root)  # 3->9->8->15->10
+    post_order(root)  # 3->9->8->14->15->10
     print()
-    in_order(root)  # 3->8->9->10->15
+    in_order(root)  # 3->8->9->10->14->15
     print()
-    pre_order(root)  # 10->8->3->9->15
+    pre_order(root)  # 10->8->3->9->15->14
     print()
 
     # search 함수에 출력 부분 제거, 리턴 값은 bool
@@ -85,3 +104,7 @@ if __name__ == "__main__":
         print(f"{number}을(를) 찾았습니다")
     else:
         print(f"{number}이(가) 존재하지 않습니다")
+
+    del_number = int(input("제거할 값 : "))
+    root = delete(root, del_number)
+    post_order(root)
